aef16ec1f8b7e17685633105ff5a7166875326a9
Kevin <kshoyer@g.hmc.edu>
2019-02-25 09:55:03 -0800

    2/25/19
    Added surface finish effects on drag

--- Flight_Dynamics/RocketEquationPackage/NoseConeDrag.m
+++ Flight_Dynamics/RocketEquationPackage/NoseConeDrag.m
@@ -85,7 +85,9 @@ title('Nose Cone Drag For different Shapes')
 xlabel('Mach')
 ylabel('Drag Coefficient')
 
-%% All in One
+%% All shapes and lengths in One
+A = csvread(fullfile('Nosecone Drag Data','Von Karman','von'),1,0);
+CD_Power_Series_25_UB = A(:,3);
 
 plot(Machdata(1:600),CD_Conical_5_LB(1:600),Machdata(1:600),CD_Conical_8_LBR(1:600), Machdata(1:600),CD_Conical_12_NB(1:600), Machdata(1:600), CD_Conical_18_UBR(1:600), Machdata(1:600),CD_Conical_25_UB(1:600),Machdata(1:600),CD_Ogive_5_LB(1:600),Machdata(1:600),CD_Ogive_8_LBR(1:600), Machdata(1:600),CD_Ogive_12_NB(1:600), Machdata(1:600), CD_Ogive_18_UBR(1:600), Machdata(1:600),CD_Ogive_25_UB(1:600),Machdata(1:600),CD_Power_Series_5_LB(1:600),Machdata(1:600),CD_Power_Series_8_LBR(1:600), Machdata(1:600),CD_Power_Series_12_NB(1:600), Machdata(1:600), CD_Power_Series_18_UBR(1:600), Machdata(1:600),CD_Power_Series_25_UB(1:600))
 legend('5 inch Conical','8 inch Conical','12 inch Conical','18 inch Conical','25 inch Conical','5 inch Ogive','8 inch Ogive','12 inch Ogive','18 inch Ogive','25 inch Ogive','5 inch Power Series','8 inch Power Series','12 inch Power Series','18 inch Power Series','25 inch Power Series')
@@ -93,5 +95,20 @@ title('Conical Nose Cone Drag Data')
 xlabel('Mach')
 ylabel('Drag Coefficient')
 
+%% Surface finish data
 
+A = csvread(fullfile('Nosecone Drag Data','Surface Finish','Power_Series_12_polished.CSV'),1,0);
+Machdata = A(:,1);
+Power_Series_12_polished = A(:,3);
 
+A = csvread(fullfile('Nosecone Drag Data','Surface Finish','Power_Series_12_smooth_paint.CSV'),1,0);
+Power_Series_12_smooth_paint = A(:,3);
+
+A = csvread(fullfile('Nosecone Drag Data','Surface Finish','Power_Series_12_very_rough.CSV'),1,0);
+Power_Series_12_very_rough = A(:,3);
+
+plot(Machdata(1:600), CD_Power_Series_12_NB(1:600), Machdata(1:600), Power_Series_12_polished(1:600),Machdata(1:600),Power_Series_12_smooth_paint(1:600), Machdata(1:600),Power_Series_12_very_rough(1:600))
+legend('Perfect Surface', 'Polished', 'smooth paint','very rough')
+title('Nose Cone Drag For different Surface Finishes')
+xlabel('Mach')
+ylabel('Drag Coefficient')
